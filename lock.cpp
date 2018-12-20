387f4b364f69d6ca5813bb63b8dd20ec2e69fab3
cannizarro <42109962+cannizarro@users.noreply.github.com>
2018-12-21 00:11:52 +0530

    Added the file I have edited locally on vscode.

---
+++ lock.cpp
@@ -0,0 +1,111 @@
+#include<conio.h>
+#include <iostream>
+#include <fstream>
+#include <windows.h>    // can learn about all function at microsoft windows docs
+using namespace std;
+
+void setColor(unsigned int colour)
+{
+    HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(handle, colour);
+}
+
+int lockFolder()
+{
+    ofstream sfolder;
+    char folder[200]={0}; 
+    char tempfolder[200]={'0'};
+    char attrib[200]={'0'};
+    setColor(10);
+    
+    cout << "\nEnter the complete path to the Folder: ";
+    cin >> folder;
+
+    strcpy(tempfolder, folder);
+
+    strcat(folder, "\\Desktop.ini");
+    
+    sfolder.open(folder, ios::out);
+    
+    sfolder << "[.shellClassInfo]" << endl;
+    sfolder << "CLSID = {63E23168-BFF7-4E87-A246-EF024425E4EC}" << endl;
+
+    sfolder.close();
+
+    strcpy(attrib, "attrib +h +s ");
+    strcat(attrib, tempfolder);
+    
+    WinExec(attrib, 11); //can also use system() but using WinExec() just for information sake.
+    
+
+    cout << "\n\n" << tempfolder << " has been locked successfully!\n";
+}
+
+int unlockFolder()
+{
+    char folder[200]; 
+    char tempfolder[200];
+    char attrib[200];
+    char del[200];
+
+    setColor(13);
+    
+    cout << "\nEnter the path of the Folder you want to protect : ";
+    cin >> folder;
+
+    strcpy(tempfolder, folder);
+
+    strcat(folder, "\\Desktop.ini");
+
+    strcpy(del, "del ");
+    strcat(del, folder);
+    int r=system(del);
+    strcpy(attrib, "attrib -h -s ");
+    strcat(attrib, tempfolder);
+    
+    system(attrib);  // same as above can use WinExec() but used this for simplicity
+    if(!(r==1))
+    cout << "\n\n" << tempfolder << " has been unlocked successfully!\n";
+    return r;
+}
+
+
+
+int main()
+{
+    int ch=0;
+    SetConsoleTitle("Folder Lock");
+    abc:
+    system("CLS"); 
+    label:
+    setColor(11);
+    cout<<"\t\tFolder Lock :";
+    cout << "\n\n" << "Options:\t\t\t\n";
+    cout << "\t======================\n";
+    cout << "\t1. - Lock Folder.\n";
+    cout << "\t2. - Unlock Folder.\n";
+    cout << "\t3. - Exit.\n";
+    cout << "\t======================  \n";
+
+    setColor(4);
+    cout << "\nEnter your choice: ";
+    cin >> ch;
+    int run;
+    switch (ch)
+    {
+    case 1:
+        lockFolder(); 
+        break;
+    case 2:
+        run=unlockFolder();
+        if(run==1)
+        goto label;
+        break;
+    case 3: exit(0); break;
+    default: cout << "\nInvalid Entry. Quitting ....\n";
+    }
+    cout<<"\nPress any key to exit\n";
+    getch();
+    setColor(7);
+    return 0;
+}
